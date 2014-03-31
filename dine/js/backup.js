0acb04780546cb746279a104d3d5a331b2eab823
sakethbsv <saketh.bsv@gmail.com>
2014-04-01 01:01:48 +0530

    bug fixes, added table, time

--- dine/js/backup.js
+++ dine/js/backup.js
@@ -24,7 +24,17 @@ $(function() {
     for( j = 0, k = orderList.length; j < k; j++) {
 		console.log(localStorage.getItem(orderList[j]));
 		data = localStorage.getItem(orderList[j]).split(',');
-		
+		/*$itemList.append(
+            "<li id='" + orderList[j] + "'>"
+            + "<span class='editable'>" 
+            + data[3]
+            + "</span> &nbsp;&nbsp;&nbsp;<span>"+data[0]+"</span>"
+			+ "&nbsp;&nbsp;&nbsp;<span>"+data[5]+"</span>"
+			+ "&nbsp;&nbsp;&nbsp;<span>"+data[2]+"</span>"
+			+ "&nbsp;&nbsp;&nbsp;<span>"+data[4]+"</span>"
+			+ "<a href='#'>X</a></li>"
+        );
+		*/
 			$itemTable.append(
             " <tr id='" + orderList[j] + "'>"
             + "<td>" 
@@ -49,14 +59,30 @@ $(function() {
         e.preventDefault();
         $.publish('/add/', []);
     });
-	
+	/*
+    // Remove todo
+    $itemList.delegate('a', 'click', function(e) {
+        var $this = $(this);
+        
+        e.preventDefault();
+        $.publish('/remove/', [$this, "list"]);
+    });
+	*/
 	$itemTable.delegate('a', 'click', function(e) {
         var $this = $(this);
         
         e.preventDefault();
         $.publish('/remove/', [$this, "table"]);
     });
-   
+    /*
+    // Sort todo
+    $itemList.sortable({
+        revert: true,
+        stop: function() {
+            $.publish('/regenerate-list/', []);
+        }
+    });
+    */
 	/*
     // Edit and save todo
     $editable.inlineEdit({
@@ -74,7 +100,17 @@ $(function() {
         $.publish('/clear-all/', []);
     });
 
-     
+    // Fade In and Fade Out the Remove link on hover
+   /* $itemTable.delegate('tr', 'mouseover mouseout', function(event) {
+        var $this = $(this).find('a');
+        
+        if(event.type === 'mouseover') {
+            $this.stop(true, true).fadeIn();
+        } else {
+            $this.stop(true, true).fadeOut();
+        }
+    });
+     */   
     // Subscribes
     $.subscribe('/add/', function() {
         if ($comment.val() !== "") {
@@ -91,7 +127,18 @@ $(function() {
             
             // Append a new list item with the value of the new todo list
 			data1 = localStorage.getItem("todo-" + i).split(',');
-		
+			/*
+		$itemList.append(
+            "<li id='todo-" + i + "'>"
+            + "<span class='editable'>" 
+            + data1[3]
+            + "</span> &nbsp;&nbsp;&nbsp;<span>"+data1[0]+"</span>"
+			+ "&nbsp;&nbsp;&nbsp;<span>"+data1[5]+"</span>"
+			+ "&nbsp;&nbsp;&nbsp;<span>"+data1[2]+"</span>"
+			+ "&nbsp;&nbsp;&nbsp;<span>"+data1[4]+"</span>"
+			+ "<a href='#'>X</a></li>"
+        );
+		*/
 		console.log("adding table entry")
 		$itemTable.append(
             " <tr id='todo-" + i + "'>"
@@ -106,7 +153,13 @@ $(function() {
 	     	+ "&nbsp;&nbsp;&nbsp;<td>"+data1[4]+"</td>"
      		+ "<td><a href='#'>X</a></td></tr>"
         );
-		
+		//alert("Added a party to waitlist");
+            /*$itemList.append(
+                "<li id='todo-" + i + "'>"
+                + "<span class='editable'>"
+                + localStorage.getItem("todo-" + i) 
+                + " </span><a href='#'>x</a></li>"
+            );*/
 
             $.publish('/regenerate-list/', []);
 
@@ -127,8 +180,26 @@ $(function() {
     });
 var gen;    
     $.subscribe('/remove/', function($this, $msg) {
-		
+		/*if($msg == "list"){
+        var parentId = $this.parent().attr('id');
+		alert(parentId);
+        
+        // Remove todo list from localStorage based on the id of the clicked parent element
+        localStorage.removeItem(
+            parentId
+        );
+        
+        // Fade out the list item then remove from DOM
+        $this.parent().fadeOut(function() { 
+            $this.parent().remove();
+            
+            $.publish('/regenerate-list/', []);
+        });
+		}
+		else if($msg == "table"){
+		*/
 		var parentId = $this.parent().parent().attr('id');
+		//alert(parentId);
         
         // Remove todo list from localStorage based on the id of the clicked parent element	
         localStorage.removeItem(

a8cfa4792863990bc9e1fb646697652960099969
Saketh BSV <saketh.bsv@gmail.com>
2014-04-08 00:53:55 +0530

    Update base.js

--- dine/js/base.js
+++ dine/js/base.js
@@ -155,7 +155,7 @@ function Sync(){
         var notifyId = $(this).parent().parent().attr('id');
 		console.log(notifyId + 'notifying');
 		var data2 = localStorage.getItem(notifyId).split(',');
-  			Notify(data2[1]);
+  		//	Notify(data2[1]);
     });
 
 	/*

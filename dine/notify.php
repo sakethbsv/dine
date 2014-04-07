d8480587927554b0d08527de2ade013c7c84a78c
Saketh BSV <saketh.bsv@gmail.com>
2014-04-08 00:44:06 +0530

    Update notify.php

--- dine/notify.php
+++ dine/notify.php
@@ -38,8 +38,32 @@ curl_setopt_array($ch, array(
     //,CURLOPT_FOLLOWLOCATION => true
 ));
 
+
 //get response
 $output = curl_exec($ch);
+
+if (curl_errno($ch)) {
+				echo "curl no success";
+
+    // this would be your first hint that something went wrong
+    die('Couldn\'t send request: ' . curl_error($ch));
+
+} else {
+    // check the HTTP status code of the request
+    $resultStatus = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    if ($resultStatus == 200) {
+        // everything went better than expected
+		echo "curl success";
+    } else {
+        // the request did not complete as expected. common errors are 4xx
+        // (not found, bad request, etc.) and 5xx (usually concerning
+        // errors/exceptions in the remote script execution)
+					echo "curl no success";
+
+        die('Request failed: HTTP status code: ' . $resultStatus);
+
+    }
+}
 
 curl_close($ch);
 

c7435a3666a2d64321a73855e3f1eecf2082c940
sakethbsv <saketh.bsv@gmail.com>
2014-04-13 21:37:54 +0530

    login

--- dine/db_connect.php
+++ dine/db_connect.php
@@ -3,7 +3,7 @@ include_once 'psl-config.php';   // As functions.php is not included
 try {
     $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-	//echo("success");
+	echo("success");
 }
 catch(Exception $e){
     die(var_dump($e));

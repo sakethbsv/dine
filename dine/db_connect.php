71bb6a92fed8d05a391e10b5be1c3c2d3f45855c
sakethbsv <saketh.bsv@gmail.com>
2014-04-13 18:25:21 +0530

    login module

---
+++ dine/db_connect.php
@@ -0,0 +1,11 @@
+<?php
+include_once 'psl-config.php';   // As functions.php is not included
+try {
+    $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	//echo("success");
+}
+catch(Exception $e){
+    die(var_dump($e));
+	echo("failure, check IC");
+}
---
+++ dine/psl-config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "tcp:d6wfefgfjk.database.windows.net,1433");     // The host you want to connect to.
+define("USER", "saketh@d6wfefgfjk");    // The database username. 
+define("PASSWORD", "Ramanamurtybv123");    // The database password. 
+define("DATABASE", "dine");    // The database name.
+
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+
+?>

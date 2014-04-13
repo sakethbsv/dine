6a4008c159d430e4880b635dad4e01602e042a11
sakethbsv <saketh.bsv@gmail.com>
2014-04-13 22:05:25 +0530

    login

--- dine/process_login.php
+++ dine/process_login.php
@@ -1,10 +1,12 @@
 <?php
 include_once 'db_connect.php';
 include_once 'functions.php';
- 
+echo "ok1"; 
 sec_session_start(); // Our custom secure way of starting a PHP session.
- 
+echo "ok2"; 
 if (isset($_POST['email'], $_POST['p'])) {
+	echo "ok3"; 
+
     $email = $_POST['email'];
     $password = $_POST['p']; // The hashed password.
  
